503faee7b3c2562b9137c869ca7db19b405df86c
Alyssa McKee <alyssa_mckee@yahoo.com>
2017-11-09 16:10:26 -0600

    mail done

---
+++ alyssa_mckee/Angular/myFirstAngularApp/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+// interface mailInterface{
+	// email: string,
+	// importance: boolean,
+	// subject: string,
+	// content: string
+// }
+
+export class AppComponent {
+  title = 'app';
+  mail  = [
+  	{email: 'bill@gates.com', importance :true, subject: 'New Windows', content: "Windows XI will launch in the year 2100" },
+	{email: 'ada@lovelace.com', importance: true, subject: 'programming', content: "Enchantress of Numbers"},
+	{email: 'john@carmac.com', importance: false, subject: "Updated Algo", content: "New algorithm for shadow volumes."}
+	{email: 'gabe@newel.com', importance: false, subject: "HL3!", content: "Just Kidding"}
+  ]
+}
